dfdb34b11569b3929ce984d43b362943e78f5f0e
Sankalp26710 <60251985+Sankalp26710@users.noreply.github.com>
2020-01-24 12:38:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SOSTest
+ app
---
+++ app/src/main/java/com/example/sostest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.sostest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView ss;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
